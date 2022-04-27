6b9b12099055658a3131cb2767279276872fab48
pavel <Currency2@>
2022-04-27 12:39:07 +0400

    task_01 done

---
+++ task_01/core.py
@@ -0,0 +1,7 @@
+class Core:
+
+    """Sample higher directory level class"""
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
---
+++ task_01/project/prg1.py
@@ -0,0 +1,5 @@
+from task_01.core import Core
+
+SamplePerson01 = Core("John", 25)
+
+print(f"{SamplePerson01.name}, {SamplePerson01.age}")
